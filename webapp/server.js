d8d202c32e44a536ef113fd679c928c7e2c843dd
m-sato <m-sato@nuc2>
2018-10-15 00:03:18 +0900

    add log

--- webapp/server.js
+++ webapp/server.js
@@ -2,6 +2,26 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+var log4js = require('log4js');
+//config
+log4js.configure({
+    appenders: {
+      system: { type: 'file', filename: 'system.log', maxLogSize: 10485760, numBackups: 3 },
+      access: { type: 'dateFile', filename: 'access.log', pattern: '-yyyy-MM-dd' } //追加
+    },
+    categories: {
+        default: {appenders:['system'], level: 'debug'},
+        web: {appenders: ['access'], level: 'info'} //追加
+    }
+});
+
+
+
+//logger
+var systemLogger = log4js.getLogger(); 
+var accessLogger = log4js.getLogger('web'); //追加
+
+
 
 // api.js(サーバサイドの処理スクリプト)の読み込み
 const api = require('./server/routes/api');
@@ -9,6 +29,9 @@ const hha = require('./server/routes/cost-history');
 
 // expressの初期化
 const app = express();
+//bind access log
+app.use(log4js.connectLogger(accessLogger));
+
 
 // 静的ファイルパスの設定
 // ビルド後のAngularのパスを設定する
@@ -38,3 +61,7 @@ const server = http.createServer(app);
  * ポート監視を開始する
  */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+//system log
+systemLogger.info("Express start");
+
